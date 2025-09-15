7037f783a3dd1ea127773ca18e21c9d3bb8217e5
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-15 06:16:47 +0300

    eventlistener for form and tasklist created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //creating eventlisteners for form and tasklist
+  const form = document.getElementById("create-task-form");
+  const tasklist = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(e){
+    e.preventDefault();//this prevents page from reladoing
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.ariaValueMax.trim();
+
+    if(taskText === "" ) return; // it ensures no empty task is added
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    tasklist.appendChild(li);
+
+    taskInput.value = ""; //this clears the input field
+  });
 });
